9383f70dc1f55a438218c22ef7dd0805ad8a06f2
joaquin <joacoetruuu@gmail.com>
2023-02-19 05:15:12 -0300

    try-catch

--- src/scripts/getSearch.js
+++ src/scripts/getSearch.js
@@ -1,142 +1,214 @@
 export async function getGrid(input){
-    const res = await fetch(`https://hypescrap-production.up.railway.app/grid/${input}`).catch(error => console.log("error", error))
+    try{
+        const res = await fetch(`https://hypescrap-production.up.railway.app/grid/${input}`).catch(error => console.log("error", error))
 
-    if(res === undefined){
+        if(res === undefined){
+            return [
+                {
+                    "title": "title",
+                    "price": "price",
+                    "img": "img",
+                    "href": "href",
+                    "storeLogo": "storeLogo"
+                }
+            ]
+        }
+        
+        const data = await res.json(); 
+    
+        return data
+
+
+    }
+    catch(error){
         return [
-            {
-                "title": "title",
-                "price": "price",
-                "img": "img",
-                "href": "href",
-                "storeLogo": "storeLogo"
-            }
+            {"status": error}
         ]
     }
-    
-    const data = await res.json(); 
 
-    return data
+
 }
 
 export async function getDigital(input){
-    const res = await fetch(`https://hypescrap-production.up.railway.app/digitalsport/${input}`).catch(error => console.log("error", error))
+
+    try{
+
+
+
+        const res = await fetch(`https://hypescrap-production.up.railway.app/digitalsport/${input}`).catch(error => console.log("error", error))
     
-    if(res === undefined){
+        if(res === undefined){
+            return [
+                {
+                    "title": "title",
+                    "price": "price",
+                    "img": "img",
+                    "href": "href",
+                    "storeLogo": "storeLogo"
+                }
+            ]
+        }
+    
+        const data = await res.json();
+    
+        return data
+
+
+    }
+    catch(error){
         return [
-            {
-                "title": "title",
-                "price": "price",
-                "img": "img",
-                "href": "href",
-                "storeLogo": "storeLogo"
-            }
+            {"status": error}
         ]
     }
 
-    const data = await res.json();
-
-    return data
 }
 
 export async function moov(input){
-    const res = await fetch(`https://hypescrap-production.up.railway.app/moov/${input}`).catch(error => console.log("error", error))
+
+    try{
+
+        const res = await fetch(`https://hypescrap-production.up.railway.app/moov/${input}`).catch(error => console.log("error", error))
     
         
-    if(res === undefined){
+        if(res === undefined){
+            return [
+                {
+                    "title": "title",
+                    "price": "price",
+                    "img": "img",
+                    "href": "href",
+                    "storeLogo": "storeLogo"
+                }
+            ]
+        }
+    
+        const data = await res.json(); 
+    
+    
+    
+        return data
+
+    }
+    catch(error){
         return [
-            {
-                "title": "title",
-                "price": "price",
-                "img": "img",
-                "href": "href",
-                "storeLogo": "storeLogo"
-            }
+            {"status": error}
         ]
     }
-
-    const data = await res.json(); 
-
-
-
-    return data
 }
 
 export async function newBalance(input){
-    const res  = await fetch(`https://hypescrap-production.up.railway.app/newbalance/${input}`).catch(error => console.log("error", error))
+    try{
+        const res  = await fetch(`https://hypescrap-production.up.railway.app/newbalance/${input}`).catch(error => console.log("error", error))
 
-    if(res === undefined){
-        return [
-            {
-                "title": "title",
-                "price": "price",
-                "img": "img",
-                "href": "href",
-                "storeLogo": "storeLogo"
-            }
-        ]
+        if(res === undefined){
+            return [
+                {
+                    "title": "title",
+                    "price": "price",
+                    "img": "img",
+                    "href": "href",
+                    "storeLogo": "storeLogo"
+                }
+            ]
+        }
+    
+    
+        const data = await res.json(); 
+    
+        return data
     }
 
-
-    const data = await res.json(); 
-
-    return data
+    catch(error){
+        return [
+            {"status": error}
+        ]
+    }
 }
 
 export async function dexter(input){
-    const res  = await fetch(`https://hypescrap-production.up.railway.app/dexter/${input}`).catch(error => console.log("error", error))
 
-    if(res === undefined){
+    try{
+        const res  = await fetch(`https://hypescrap-production.up.railway.app/dexter/${input}`).catch(error => console.log("error", error))
+
+        if(res === undefined){
+            return [
+                {
+                    "title": "title",
+                    "price": "price",
+                    "img": "img",
+                    "href": "href",
+                    "storeLogo": "storeLogo"
+                }
+            ]
+        }
+    
+        const data = await res.json(); 
+    
+        return data
+    }
+    catch(error){
         return [
-            {
-                "title": "title",
-                "price": "price",
-                "img": "img",
-                "href": "href",
-                "storeLogo": "storeLogo"
-            }
+            {"status": error}
         ]
     }
 
-    const data = await res.json(); 
-
-    return data
 }
 
 export async function funcarral(input){
-    const res  = await fetch(`https://hypescrap-production.up.railway.app/funcarral/${input}`).catch(error => console.log("error", error))
+    try{
+
+        const res  = await fetch(`https://hypescrap-production.up.railway.app/funcarral/${input}`).catch(error => console.log("error", error))
     
-    if(res === undefined){
+        if(res === undefined){
+            return [
+                {
+                    "title": "title",
+                    "price": "price",
+                    "img": "img",
+                    "href": "href",
+                    "storeLogo": "storeLogo"
+                }
+            ]
+        }
+        const data = await res.json(); 
+    
+        return data
+
+    }
+    catch(error){
         return [
-            {
-                "title": "title",
-                "price": "price",
-                "img": "img",
-                "href": "href",
-                "storeLogo": "storeLogo"
-            }
+            {"status": error}
         ]
     }
-    const data = await res.json(); 
 
-    return data
 }
 
 export async function drops(input){
-    const res  = await fetch(`https://hypescrap-production.up.railway.app/drops/${input}`).catch(error => console.log("error", error))
+    try{
 
-    if(res === undefined){
+        const res  = await fetch(`https://hypescrap-production.up.railway.app/drops/${input}`).catch(error => console.log("error", error))
+
+        if(res === undefined){
+            return [
+                {
+                    "title": "title",
+                    "price": "price",
+                    "img": "img",
+                    "href": "href",
+                    "storeLogo": "storeLogo"
+                }
+            ]
+        }
+    
+        const data = await res.json(); 
+    
+        return data
+
+    }
+    catch(error){
         return [
-            {
-                "title": "title",
-                "price": "price",
-                "img": "img",
-                "href": "href",
-                "storeLogo": "storeLogo"
-            }
+            {"status": error}
         ]
     }
 
-    const data = await res.json(); 
-
-    return data
 }
