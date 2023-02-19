44f499a0f22ede8ff4f2460dcb813b0fce19f965
joaquin <joacoetruuu@gmail.com>
2023-02-19 04:47:21 -0300

    error Lense

--- src/scripts/getSearch.js
+++ src/scripts/getSearch.js
@@ -1,47 +1,74 @@
 export async function getGrid(input){
     const res = await fetch(`https://hypescrap-production.up.railway.app/grid/${input}`).catch(error => console.log("error", error))
-    const data = await res.json(); 
-    if(data === undefined){
-        return "<p>Error</p>"
+
+    if(res === undefined){
+        return [{
+            "status": "404"
+        }]
     }
+    
+    const data = await res.json(); 
+
     return data
 }
 
 export async function getDigital(input){
     const res = await fetch(`https://hypescrap-production.up.railway.app/digitalsport/${input}`).catch(error => console.log("error", error))
-    const data = await res.json();
-    if(data === undefined){
-        return "<p>Error</p>"
+    
+    if(res === undefined){
+        return [{
+            "status": "404"
+        }]
     }
+
+    const data = await res.json();
+
     return data
 }
 
 export async function moov(input){
     const res = await fetch(`https://hypescrap-production.up.railway.app/moov/${input}`).catch(error => console.log("error", error))
+    
+        
+    if(res === undefined){
+        return [{
+            "status": "404"
+        }]
+    }
+
     const data = await res.json(); 
 
-    if(data === undefined){
-        return "<p>Error</p>"
-    }
+
 
     return data
 }
 
 export async function newBalance(input){
     const res  = await fetch(`https://hypescrap-production.up.railway.app/newbalance/${input}`).catch(error => console.log("error", error))
-    const data = await res.json(); 
-    if(data === undefined){
-        return "<p>Error</p>"
+
+    if(res === undefined){
+        return [{
+            "status": "404"
+        }]
     }
+
+
+    const data = await res.json(); 
+
     return data
 }
 
 export async function dexter(input){
     const res  = await fetch(`https://hypescrap-production.up.railway.app/dexter/${input}`).catch(error => console.log("error", error))
-    const data = await res.json(); 
-    if(data === undefined){
-        return "<p>Error</p>"
+
+    if(res === undefined){
+        return [{
+            "status": "404"
+        }]
     }
+
+    const data = await res.json(); 
+
     return data
 }
 
@@ -49,16 +76,23 @@ export async function funcarral(input){
     const res  = await fetch(`https://hypescrap-production.up.railway.app/funcarral/${input}`).catch(error => console.log("error", error))
     const data = await res.json(); 
     if(data === undefined){
-        return "<p>Error</p>"
+        return [{
+            "status": "404"
+        }]
     }
     return data
 }
 
 export async function drops(input){
     const res  = await fetch(`https://hypescrap-production.up.railway.app/drops/${input}`).catch(error => console.log("error", error))
-    const data = await res.json(); 
-    if(data === undefined){
-        return "<p>Error</p>"
+
+    if(res === undefined){
+        return [{
+            "status": "404"
+        }]
     }
+
+    const data = await res.json(); 
+
     return data
 }
