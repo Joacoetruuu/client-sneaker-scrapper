a7edf37b9cf210a0e4601a27799520750ea7f7ba
joaquin <joacoetruuu@gmail.com>
2023-02-20 09:07:24 -0300

    skeleton Center

--- src/components/Recomendados/SkeletonRecomendados.jsx
+++ src/components/Recomendados/SkeletonRecomendados.jsx
@@ -4,35 +4,59 @@ import { Text, Center, Box, Stack, SkeletonText } from '@chakra-ui/react'
 export function SkeletonRecomendados() {
   return (
     <>
-      <div className="cardLanding">
+          <Center>
+
+                <div className="cardLanding">
+            
+            <Stack>
+              <SkeletonText mt="4" noOfLines={10} spacing="5" skeletonHeight="2" />
+            </Stack>  
+          </div>
+
+      </Center>
+
+
+      <Center>
+      <div className="cardLanding" margin="auto">
+        <Stack>
+          <SkeletonText mt="4" noOfLines={10} spacing="5" skeletonHeight="2" />
+        </Stack>
+      </div>
+      </Center>
+
+      <Center>
+
+      <div className="cardLanding" margin="auto">
         <Stack>
           <SkeletonText mt="4" noOfLines={10} spacing="5" skeletonHeight="2" />
         </Stack>
       </div>
 
-      <div className="cardLanding">
+      </Center>
+
+
+      <Center>
+
+      <div className="cardLanding" margin="auto">
         <Stack>
           <SkeletonText mt="4" noOfLines={10} spacing="5" skeletonHeight="2" />
         </Stack>
       </div>
 
-      <div className="cardLanding">
+      </Center>
+
+      <Center>
+
+        
+      <div className="cardLanding" margin="auto">
         <Stack>
           <SkeletonText mt="4" noOfLines={10} spacing="5" skeletonHeight="2" />
         </Stack>
       </div>
 
-      <div className="cardLanding">
-        <Stack>
-          <SkeletonText mt="4" noOfLines={10} spacing="5" skeletonHeight="2" />
-        </Stack>
-      </div>
+      </Center>
 
-      <div className="cardLanding">
-        <Stack>
-          <SkeletonText mt="4" noOfLines={10} spacing="5" skeletonHeight="2" />
-        </Stack>
-      </div>
+      
     </>
   );
 }
